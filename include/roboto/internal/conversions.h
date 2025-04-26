b5a859b0e35dc0c226d951f3586f9f561e5f6b0a
paulys0 <s17spaul@uni-bonn.de>
2025-04-26 22:08:17 +0200

    Optimize conversions for radians

--- include/roboto/internal/conversions.h
+++ include/roboto/internal/conversions.h
@@ -6,8 +6,30 @@
 namespace roboto {
 namespace internal {
 
+// Precomputed constants
+constexpr double DEG_TO_RAD = M_PI / (360.0 / 2.0);
+constexpr double RAD_TO_DEG = (360.0 / 2.0) / M_PI;
+
+// Fast conversions
+
+/// @brief Converts Radian to Degree
+/// @param num Radian value
+/// @return Degree value (does not check for out of range)
+inline constexpr double RadToDeg(const double &rad) {
+  // rad -> deg: rad * (360.0 / 2) / M_PI
+  return rad * RAD_TO_DEG;
+}
+
+/// @brief Converts Degree to Radian.
+/// @param deg Degree value
+/// @return Radian value (does not check for out of range)
+inline constexpr double DegToRad(const double &deg) {
+  // deg -> rad: deg * (M_PI / (360.0 / 2))
+  return deg * DEG_TO_RAD;
+}
+
 /// @brief Converts laser number to degree. Based on Laser configuration set in
-/// config.h. Faster than LaserNumToRad.
+/// config.h.
 /// @param num laser number ranging from 0 to NUM_LAZERS
 /// @return degree value (does not check for out of range)
 inline constexpr double LaserNumToDeg(const double &num) {
@@ -16,39 +38,22 @@ inline constexpr double LaserNumToDeg(const double &num) {
 }
 
 /// @brief Converts laser number to radian. Based on Laser configuration set in
-/// config.h Slower than DegToRad.
+/// config.h
 /// @param num laser number ranging from 0 to NUM_LAZERS
 /// @return radian value (does not check for out of range)
 inline constexpr double LaserNumToRad(const double &num) {
-  // num -> rad: deg * (M_PI / (360.0 / 2))
-  return LaserNumToDeg(num) * M_PI / (360.0 / 2);
-}
-
-/// @brief Converts Radian to Degree
-/// @param num Radian value
-/// @return Degree value (does not check for out of range)
-inline const double RadToDeg(const double &rad) {
-  // rad -> deg: rad * (360.0 / 2) / M_PI
-  return rad * (360.0 / 2) / M_PI;
+  // num -> rad: look at LaserNumToDeg
+  return MIN_LAZER_RAD + num * ANGLE_OF_LASER_RADIAN;
 }
 
 /// @brief Converts Radian to laser number. Based on Laser configuration set in
 /// config.h. Slower than DegToLaserNum.
 /// @param rad Radian value
 /// @return laser number (does not check for out of range)
-inline const int RadToLaserNum(const double &rad) {
+inline constexpr int RadToLaserNum(const double &rad) {
   // rad -> num: (deg - internal::MIN_LAZER_DEG) /
   // internal::ANGLE_OF_LASER_DEGREE
-  return static_cast<int>((RadToDeg(rad) - MIN_LAZER_DEG) /
-                          ANGLE_OF_LASER_DEGREE);
-}
-
-/// @brief Converts Degree to Radian.
-/// @param deg Degree value
-/// @return Radian value (does not check for out of range)
-inline constexpr double DegToRad(const double &deg) {
-  // deg -> rad: deg * (M_PI / (360.0 / 2))
-  return deg * M_PI / (360.0 / 2);
+  return static_cast<int>(rad - MIN_LAZER_RAD) / ANGLE_OF_LASER_RADIAN;
 }
 
 /// @brief Converts Degree to laser number. Based on Laser configuration set in
